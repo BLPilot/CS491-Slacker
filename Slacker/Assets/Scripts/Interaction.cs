f242901fcf67a21add608335ee406a2fa97fbac6
Haydrill <saldua.h@gmail.com>
2022-04-11 23:13:15 -0700

    updated level and door to have locks

--- Slacker/Assets/Scripts/Interaction.cs
+++ Slacker/Assets/Scripts/Interaction.cs
@@ -12,11 +12,25 @@ public class Interaction : MonoBehaviour
 
     public bool inTrigger;
 
+    public float timeToUnlock;
+    public float doorCooldownTime;
+
+    public float nextUnlockTimer = 0;
+    public float doorCooldownTimer = 0;
+
+    bool cooldownActive = false;
+
+    public GameObject lockIcon;
+    public GameObject unlockedIcon;
+    public GameObject cooldownIcon;
+
     // Start is called before the first frame update
     void Start()
     {
         active = false;
         inTrigger = false;
+
+
     }
 
     // Update is called once per frame
@@ -24,8 +38,33 @@ public class Interaction : MonoBehaviour
     {
         if (inTrigger && (Input.GetButtonUp("Interact") || Input.GetButtonUp("Fire1") ) )
         {
-            Debug.Log("Interacting");
+            //Debug.Log("Interacting");
             Interact();
+        }
+
+        if (nextUnlockTimer > 0) { nextUnlockTimer -= Time.deltaTime; };
+        if (doorCooldownTimer > 0) { doorCooldownTimer -= Time.deltaTime; };
+
+        if (active && doorCooldownTimer <= 0 && nextUnlockTimer <= 0)
+        {
+            active = false;
+            doorCooldownTimer = doorCooldownTime;
+
+            var rotationVector = interactible.transform.rotation.eulerAngles;
+            Debug.Log("cooldown started");
+            rotationVector.y -= 90;
+            //Debug.Log(rotationVector);
+            interactible.transform.rotation = Quaternion.Euler(rotationVector);
+            interactible.layer = LayerMask.NameToLayer("Default");
+
+            // Show the icons
+            lockIcon.SetActive(false);
+            cooldownIcon.SetActive(true);
+        }
+        else if (!active && doorCooldownTimer <= 0 && nextUnlockTimer <= 0)
+        {
+            cooldownIcon.SetActive(false);
+            unlockedIcon.SetActive(true);
         }
     }
 
@@ -47,25 +86,52 @@ public class Interaction : MonoBehaviour
 
     void Interact()
     {
+        /*
         if (active)
         {
             var rotationVector = interactible.transform.rotation.eulerAngles;
-            Debug.Log(rotationVector);
+            //Debug.Log("ACTIVE -> INACTIVE");
             rotationVector.y += 90;
-            Debug.Log(rotationVector);
+            //Debug.Log(rotationVector);
             interactible.transform.rotation = Quaternion.Euler(rotationVector);
             interactible.layer = LayerMask.NameToLayer("Wall");
         }
         else
         {
             var rotationVector = interactible.transform.rotation.eulerAngles;
-            Debug.Log(rotationVector);
+            //Debug.Log(rotationVector);
             rotationVector.y -= 90;
-            Debug.Log(rotationVector);
+            //Debug.Log(rotationVector);
             interactible.transform.rotation = Quaternion.Euler(rotationVector);
             interactible.layer = LayerMask.NameToLayer("Wall");
         }
         active = !active;
+        */
 
+        if (nextUnlockTimer <= 0 && doorCooldownTimer <= 0)
+        {
+            // Lock the door
+
+            var rotationVector = interactible.transform.rotation.eulerAngles;
+            Debug.Log("LETSA GO");
+            rotationVector.y += 90;
+            //Debug.Log(rotationVector);
+            interactible.transform.rotation = Quaternion.Euler(rotationVector);
+            interactible.layer = LayerMask.NameToLayer("Wall");
+
+            // Show the lock Icon
+            unlockedIcon.SetActive(false);
+            lockIcon.SetActive(true);
+
+            nextUnlockTimer = timeToUnlock;
+
+            active = true;
+        }
+
+    }
+
+    void DoorCooldown()
+    {
+        
     }
 }
